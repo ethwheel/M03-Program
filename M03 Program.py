c1cd96b8a83b8717eba78dbbbb21fb130ea8414a
Ethan Wheeler <ethan.wheeler1997@gmail.com>
2024-06-25 20:10:16 -0400

    Working Class

--- M03 Program.py
+++ M03 Program.py
@@ -1,10 +1,9 @@
 class Vehicle():
     def __init__(self,type):
-        self.type=type
-class Automobile(Vehicle):
-    def __init__(self,type,make,model,doors,roof):
-        self.type=type
-        self.make=make
-        self.model=model
-        self.doors=doors
-        self.roof=roof
+        self.type = type
+    def setType(self):
+       self.type = input("Type a number: ")
+vic = Vehicle(0)
+for loop in range(2):
+    vic.setType()
+    print(vic.type)
