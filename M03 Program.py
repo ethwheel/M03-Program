65d5d59e9aaa179ed99dd10932c9d54e855ba404
Ethan Wheeler <ethan.wheeler1997@gmail.com>
2024-06-25 20:49:29 -0400

    :Nested class, needs bugfix

--- M03 Program.py
+++ M03 Program.py
@@ -2,8 +2,46 @@ class Vehicle():
     def __init__(self,type):
         self.type = type
     def setType(self):
-       self.type = input("Type a number: ")
+       self.type = input("What type of vehicle is it?: ")
+
+class Automobile(Vehicle):
+    def __init__(self, type, year, make, model, door, roof):
+        super().__init__(type)
+    def setYear(self):
+        self.year = input("What year is your vehicle?: ")
+    def setMake(self):
+        self.make = input("What make is it?: ")
+    def setModel(self):
+        self.model = input("What model?: ")
+    def setDoor(self):
+        self.door = input("How many doors does it have?: ")
+    def setRoof(self):
+        self.roof = input("Does it have a sun roof? (Y/N): ")
+        if input == "Y":
+            self.roof = "Sun roof"
+        else:
+            self.roof = "Solid"
+
+
+
+
+
+
 vic = Vehicle(0)
-for loop in range(2):
+for loop in range (1):
     vic.setType()
-    print(vic.type)
+print(vic.setType)
+vic2 = Automobile(0,0,0,0,0,0)
+for loop in range(1):
+    vic2.setYear
+    vic2.setMake
+    vic2.setModel
+    vic2.setDoor
+    vic2.setRoof
+    print("Vehicle type: ",vic2.setType,)
+    print("Year: ",vic2.setYear)
+    print("Make: ",vic2.setMake)
+    print("Model: ",vic2.setModel)
+    print("Number of doors: ",vic2.setDoor)
+    print("Type of roof: ",vic2.setRoof)
+    
